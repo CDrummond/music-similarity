9545c04249b9c396c6624004b6aa02fbbe4a091e
Craig Drummond <craig.p.drummond@gmail.com>
2021-09-26 06:52:59 +0100

    Try to increase randomness

--- lib/app.py
+++ lib/app.py
@@ -19,7 +19,7 @@ MAX_TRACKS_TO_RETURN                  = 50   # Max value for 'count' parameter
 DEFAULT_NUM_PREV_TRACKS_FILTER_ARTIST = 15   # Try to ensure artist is not in previous N tracks
 DEFAULT_NUM_PREV_TRACKS_FILTER_ALBUM  = 25   # Try to ensure album is not in previous N tracks
 NUM_SIMILAR_TRACKS_FACTOR             = 25   # Request count*NUM_SIMILAR_TRACKS_FACTOR from musly
-SHUFFLE_FACTOR                        = 1.75 # How many (shuffle_factor*count) tracks to shuffle?
+SHUFFLE_FACTOR                        = 2    # How many (shuffle_factor*count) tracks to shuffle?
 MIN_MUSLY_NUM_SIM                     = 5000 # Min number of tracs to query musly for
 
 class SimilarityApp(Flask):
@@ -433,6 +433,7 @@ def similar_api():
         _LOGGER.debug('Acceptable genres: %s' % acceptable_genres)
 
     similarity_count = int(count * SHUFFLE_FACTOR) if shuffle and (count<20 or len(track_ids)<10) else count
+    tracks_per_seed = int(similarity_count*2.5) if similarity_count<15 else similarity_count
 
     num_sim = count * len(track_ids) * 50
     if num_sim<MIN_MUSLY_NUM_SIM:
@@ -529,7 +530,7 @@ def similar_api():
                     # Save mapping of this ID to its position in similar_tracks so that we can determine if we have
                     # seen this track before.
                     similar_track_positions[simtrack['id']]=len(similar_tracks)-1
-                    if accepted_tracks>=similarity_count:
+                    if accepted_tracks>=tracks_per_seed:
                         break
 
     # For each matched_artists randomly select a track...
