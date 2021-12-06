681531f0d8e07f0e01caeb72cb58c78ed9942c5b
Craig Drummond <craig.p.drummond@gmail.com>
2021-12-06 17:14:04 +0000

    Create multiple ZIPs for release

--- mkrel.py
+++ mkrel.py
@@ -65,28 +65,40 @@ def resetVersion():
     subprocess.call(['git', 'checkout', os.path.join('lib', 'version.py')], shell=False)
 
 
+def createOtherZip(config, libs, name):
+    if config is not None:
+        os.rename("%s/config.json" % APP_NAME, "%s/config-orig.json" % APP_NAME)
+        os.rename("%s/configs/%s.json" % (APP_NAME, config), "%s/config.json" % APP_NAME)
+    cmd=["zip", "-r", "%s/%s-%s-%s.zip" % (APP_NAME, APP_NAME, name, version)]
+    for f in ["ChangeLog", "config.json", "LICENSE", "%s.py" % APP_NAME, "README.md", "requirements.txt"]:
+        cmd.append("%s/%s" % (APP_NAME, f))
+    if name == 'pi':
+        cmd.append("%s.service" % APP_NAME)
+    for e in os.listdir("%s/lib" % APP_NAME):
+        if e.endswith(".py"):
+            cmd.append("%s/lib/%s" % (APP_NAME, e))
+    for lib in libs:
+        cmd.append("%s/lib/%s" % (APP_NAME, lib))
+    subprocess.call(cmd, shell=False)
+    if config is not None:
+        os.rename("%s/config.json" % APP_NAME, "%s/configs/%s.json" % (APP_NAME, config))
+        os.rename("%s/config-orig.json" % APP_NAME, "%s/config.json" % APP_NAME)
+
+
 def createZip(version):
     info("Creating ZIPs")
     os.chdir('..')
     cmd=["zip", "-r", "%s/%s-all-%s.zip" % (APP_NAME, APP_NAME, version), "%s/essentia" % APP_NAME]
-    for f in ["ChangeLog", "config.json", "LICENSE", "%s.py" % APP_NAME, "%s.service" % APP_NAME, "README.md", "scripts", "docs"]:
+    for f in ["ChangeLog", "config.json", "LICENSE", "%s.py" % APP_NAME, "%s.service" % APP_NAME, "README.md", "requirements.txt", "scripts", "docs"]:
         cmd.append("%s/%s" % (APP_NAME, f))
     for e in os.listdir("%s/lib" % APP_NAME):
         if e.endswith(".py") or e in ["armv7l", "x86-64"]:
             cmd.append("%s/lib/%s" % (APP_NAME, e))
     subprocess.call(cmd, shell=False)
-    os.rename("%s/config.json" % APP_NAME, "%s/config-orig.json" % APP_NAME)
-    os.rename("%s/config-pi.json" % APP_NAME, "%s/config.json" % APP_NAME)
-    cmd=["zip", "-r", "%s/%s-pi-%s.zip" % (APP_NAME, APP_NAME, version)]
-    for f in ["ChangeLog", "config.json", "LICENSE", "%s.py" % APP_NAME, "%s.service" % APP_NAME, "README.md"]:
-        cmd.append("%s/%s" % (APP_NAME, f))
-    for e in os.listdir("%s/lib" % APP_NAME):
-        if e.endswith(".py"):
-            cmd.append("%s/lib/%s" % (APP_NAME, e))
-    cmd.append("%s/lib/armv7l/raspbian-buster/libmusly.so" % APP_NAME)
-    subprocess.call(cmd, shell=False)
-    os.rename("%s/config.json" % APP_NAME, "%s/config-pi.json" % APP_NAME)
-    os.rename("%s/config-orig.json" % APP_NAME, "%s/config.json" % APP_NAME)
+    createOtherZip("pi", ["linux/armv7l/raspbian-buster/libmusly.so"], "pi")
+    createOtherZip(None, ["linux/x86-64/fedora/libmusly.so"], "linux")
+    createOtherZip("windows-32", ["windows/mingw32/libmusly.dll", "windows/mingw32/libgcc_s_dw2-1.dll", "windows/mingw32/libstdc++-6.dll"], "windows32")
+    createOtherZip("windows-64", ["windows/mingw64/libmusly.dll", "windows/mingw64/libgcc_s_seh-1.dll", "windows/mingw64/libstdc++-6.dll"], "windows64")
     os.chdir(APP_NAME)
 
 
