31a5fee5470439f478ab1ec912f225d2c7bdb14c
Craig Drummond <craig.p.drummond@gmail.com>
2022-02-10 20:57:15 +0000

    Fix musly test

--- lib/test.py
+++ lib/test.py
@@ -32,9 +32,7 @@ def test_jukebox(app_config, jukebox_path, repeat):
             mus.write_jukebox(jukebox_path)
             meta_db.close()
 
-        mta=musly.MuslyTracksAdded(paths, tracks, ids)
-
-        simtracks = mus.get_similars( mta.mtracks, mta.mtrackids, 0, 100 )
+        simtracks = mus.get_similars( tracks, ids, 0, 100 )
         if len(simtracks)<2:
             _LOGGER.error('Too few tracks returned from similarity query???')
         else:
